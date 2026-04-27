18ff522c46f3581ea8f445407fe220e56a84b460
Davi Fernandes <davi.fer159@gmail.com>
2026-04-27 14:01:13 -0300

    docs: create docs of  button component with storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,28 @@
+import type { Preview } from "@storybook/react-vite";
+
+import "../src/index.css";
+import "./storybook.css";
+
+const preview: Preview = {
+  parameters: {
+    backgrounds: {
+      values: [{ name: "light", value: "ffffff" }],
+      default: "light",
+    },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+
+    a11y: {
+      // 'todo' - show a11y violations in the test UI only
+      // 'error' - fail CI on a11y violations
+      // 'off' - skip a11y checks entirely
+      test: "todo",
+    },
+  },
+};
+
+export default preview;
