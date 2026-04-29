f853e011065fda4db81070d6325004574391d545
Davi Fernandes <davi.fer159@gmail.com>
2026-04-29 14:18:53 -0300

    feat: create edit tasks page

--- src/components/add-task-dialog.tsx
+++ src/components/add-task-dialog.tsx
@@ -14,29 +14,45 @@ export const AddTaskDialog = ({
   if (!isDialogOpen) return;
 
   return (
-    <div className="fixed top-0 left-0 flex h-screen w-screen items-center justify-center bg-black/20">
-      <div className="w-full max-w-84 space-y-4 rounded-lg bg-white p-5">
+    <dialog className="fixed top-0 left-0 flex h-screen w-screen items-center justify-center bg-black/20">
+      <div className="w-full max-w-93 space-y-4 rounded-lg bg-white p-5">
         <div className="flex flex-col gap-1 text-center">
           <h2 className="text-dark-blue text-2xl font-semibold">Nova Tarefa</h2>
           <p className="text-dark-gray text-sm">Insira as informações abaixo</p>
         </div>
-        <Input labelText="Título" placeholder="Título da tarefa" />
-        <InputSelect labelText="Horário" />
-        <Input labelText="Descrição" placeholder="Descrição da tarefa" />
+        <Input
+          labelText="Título"
+          placeholder="Título da tarefa"
+          aria-label="Título da tarefa"
+        />
+        <InputSelect labelText="Horário" aria-label="Título da tarefa" />
+        <Input
+          labelText="Descrição"
+          placeholder="Descrição da tarefa"
+          aria-label="Descrição da tarefa"
+        />
         <div className="flex items-center gap-3">
           <Button
             variant="cancel"
             size="lg"
             className="w-full"
             onClick={handleToggleDialog}
+            aria-label="Cancelar"
+            title="Cancelar"
           >
             Cancelar
           </Button>
-          <Button variant="default" size="lg" className="w-full">
-            Entrar
+          <Button
+            variant="default"
+            size="lg"
+            className="w-full"
+            aria-label="Criar tarefa"
+            title="Criar tarefa"
+          >
+            Salvar
           </Button>
         </div>
       </div>
-    </div>
+    </dialog>
   );
 };
--- src/routes/main-router.tsx
+++ src/routes/main-router.tsx
@@ -4,6 +4,7 @@ import Register from "../pages/register";
 import Login from "../pages/login";
 import Tasks from "../pages/tasks";
 import { Sidebar } from "../components/sidebar";
+import EditTask from "../pages/edit-task";
 
 const MainRouter = () => {
   return (
@@ -14,7 +15,7 @@ const MainRouter = () => {
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<Dashboard />} />
         <Route path="/tasks" element={<Tasks />} />
-        <Route path="/edit-task" element={<Tasks />} />
+        <Route path="/tasks/:id" element={<EditTask />} />
       </Routes>
     </BrowserRouter>
   );
