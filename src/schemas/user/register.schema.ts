b408d8b2719edac1e925bd3206638087cd5436a9
Davi Fernandes <davi.fer159@gmail.com>
2026-05-01 11:58:46 -0300

    feat: added trim in schemas

--- src/schemas/user/register.schema.ts
+++ src/schemas/user/register.schema.ts
@@ -4,8 +4,9 @@ export const registerUserSchema = z
   .object({
     username: z
       .string()
-      .min(3, "O nome do usuário deve ter no mínimo 3 caracteres"),
-    email: z.email("Email inválido").min(1, "O email é obrigatório"),
+      .min(3, "O nome do usuário deve ter no mínimo 3 caracteres")
+      .trim(),
+    email: z.email("Email inválido").min(1, "O email é obrigatório").trim(),
     password: z.string().min(6, "A senha deve ter no mínimo 6 caracteres"),
     confirmPassword: z
       .string()
